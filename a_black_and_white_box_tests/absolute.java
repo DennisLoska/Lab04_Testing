2553c9a7f0b19729390d910f746ae8d5048f4bd1
tony-dorfmeister <tony.dorfmeister@me.com>
2017-12-13 08:26:21 +0100

    implemented white-box tests for absolute

--- a_black_and_white_box_tests/absolute.java
+++
@@ -1,9 +0,0 @@
-// Code for White-box test exercise
-/*
-public static int absoluteValueOf (int x) {
-  if (x < -1)
-     return -x;
-  else
-     return x;
-}
-*/
---
+++ a_black_and_white_box_tests/src/absolute.java
@@ -0,0 +1,11 @@
+// Code for White-box test exercise
+
+public class absolute{
+
+    public static int absoluteValueOf(int x) {
+        if (x < -1)
+            return -x;
+        else
+            return x;
+    }
+}
